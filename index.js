88723b6e4776c7efae5745d6d90b64df70a281a8
vanessa082 <wahvanessa22@gmail.com>
2024-02-14 11:55:17 +0100

    created the buy now section in the index.js and copied the styling from bunow.css to the public css folder

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 import { createNavBar } from "./src/components/navbar/navbar.js";
 
 createNavBar(true); // is index page is true
+
+const createBuyNow = (isIndexPage = false) => {
+  const baseUrl = isIndexPage ? '.' : '../../..'; // root path
+
+  const temp_late = `
+<div class="left-side">
+    <h1>Grab Upto 50% Off On <br>Selected Headphone</h1>
+    <button id="buy-btn">Buy Now</button>
+</div>
+<img src="${baseUrl}/public/image/backgroundimg.svg" alt="avatar image" />`;
+
+const buyNow = document.getElementById('buy-now');
+
+buyNow.innerHTML = temp_late;
+}
+
+createBuyNow(true)

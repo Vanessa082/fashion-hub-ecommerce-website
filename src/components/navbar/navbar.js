8a4523e5d1ba7cb3e49efb3029bf8a7c0a38f17d
vanessa082 <wahvanessa22@gmail.com>
2024-02-14 05:57:43 +0100

    added check for github host to use absolute path

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,48 +1,50 @@
-const template = `
-  <div class="left-nav">
-    <h1>FashionHub</h1>
-    <ul>
-      <li class="active">
-        Category
-        <div style="width: auto">
-          <div style="float: left; width: 10%">
-            <ul class="subnavbar">
-              <li class="active"><a href="#">Men's Categories:</a></li>
-              <li class="active"><a href="#">Tops</a></li>
-              <li class="active"><a href="#">Bottoms</a></li>
-              <li class="active"><a href="#">Shoes and accessories</a></li>
-            </ul>
-          </div>
-          <div style="float: right; width: 10%">
-            <ul class="subnavbar">
-              <li class="active"><a href="#">Women's Categories:</a></li>
-              <li class="active"><a href="#">Tops</a></li>
-              <li class="active"><a href="#">Bottoms</a></li>
-              <li class="active"><a href="#">Shoes and accessories</a></li>
-            </ul>
-          </div>
-        </div>
-      </li>
-      <li>Brand</li>
-      <li><a href='./src/pages/contact/contact.html'>Contact</a></li>
-      <li>FAQs</li>
-    </ul>
-  </div>
+const createNavBar = () => {
+  const baseUrl = window.location.host.includes('github') ? 'fashion-hub-ecommerce-website' : '';
 
-  <div class="right-nav">
-    <img src="/public/image/card.svg" alt="shopping-card" class="shopping-card">
-    <img src="/public/image/notification.svg" alt="notification-icon" class="notification-icon">
-    <div class="user-profile">
-      <img src="/public/image/profile.svg" alt="user-profile">
-      <div class="details">
-        <span class="greetings">Good morning!</span>
-        <span class="username">Scarlet Johnson</span>
+  const template = `
+    <div class="left-nav">
+      <h1>FashionHub</h1>
+      <ul>
+        <li class="active">
+          Category
+          <div style="width: auto">
+            <div style="float: left; width: 10%">
+              <ul class="subnavbar">
+                <li class="active"><a href="#">Men's Categories:</a></li>
+                <li class="active"><a href="#">Tops</a></li>
+                <li class="active"><a href="#">Bottoms</a></li>
+                <li class="active"><a href="#">Shoes and accessories</a></li>
+              </ul>
+            </div>
+            <div style="float: right; width: 10%">
+              <ul class="subnavbar">
+                <li class="active"><a href="#">Women's Categories:</a></li>
+                <li class="active"><a href="#">Tops</a></li>
+                <li class="active"><a href="#">Bottoms</a></li>
+                <li class="active"><a href="#">Shoes and accessories</a></li>
+              </ul>
+            </div>
+          </div>
+        </li>
+        <li>Brand</li>
+        <li><a href='${baseUrl}/src/pages/contact/contact.html'>Contact</a></li>
+        <li>FAQs</li>
+      </ul>
+    </div>
+
+    <div class="right-nav">
+      <img src="${baseUrl}/public/image/card.svg" alt="shopping-card" class="shopping-card">
+      <img src="${baseUrl}/public/image/notification.svg" alt="notification-icon" class="notification-icon">
+      <div class="user-profile">
+        <img src="${baseUrl}/public/image/profile.svg" alt="user-profile">
+        <div class="details">
+          <span class="greetings">Good morning!</span>
+          <span class="username">Scarlet Johnson</span>
+        </div>
       </div>
     </div>
-  </div>
 `;
 
-const createNavBar = () => {
   const navBar = document.getElementById('navbar');
 
   navBar.innerHTML = template;
